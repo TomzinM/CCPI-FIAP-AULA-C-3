63c791d2d2351497468120c4ae252fe7938cdb4d
Tom Morais <82970353+TomzinM@users.noreply.github.com>
2026-09-17 09:45:14 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,43 +1,85 @@
 #include <stdio.h>
-#include <string.h>     
 
-int main(){
+int contagem(int n)
+{
+    // base
 
-    char produtos[10][30];
-    char resposta = 'y';
-    char busca[100];
-    int achado;
-
-    while (resposta == 'y' || resposta == 'Y'){
-        for (int i = 0; i < 10; i++){
-            printf("Qual o nome do produto %d? ", i+1);
-            fgets(produtos[i], sizeof(produtos[i]), stdin);
-        }
-        for (int i = 0; i < 10; i++){
-        printf("%s", produtos[i]);
-        }
-
-        printf("Quer reescrever os produtos? (Y/N)");
-        scanf("%c", &resposta);
-        while ((getchar()) != '\n'); 
-
-    }   
-
-    printf("Qual produto quer buscar?");
-    fgets(busca, sizeof(busca), stdin);
-
-    for (int i = 0; i < 10; i++){
-        if (strcmp(busca, produtos[i]) == 0){
-        achado = 1;
-      }
+    if (n == 0)
+    {
+        printf("Fim.\n\n");
+        return 0;
     }
 
-    if (achado == 1){ 
-        printf("Produto em estoque.");
+    // recursivo
+    printf("%d\n", n);
+    return contagem(n - 1);
+}
+
+int somatorio(int n){
+    if (n <= 0){
+        return 0;
     }
-    else{
-        printf("Produto nao encontrado.");
+    
+
+    return n + somatorio(n - 1); 
+}
+
+long long potencia(int base, int exponente){
+
+    if (exponente == 0){
+        return 1;
     }
-     
+
+    return base * potencia(base, exponente - 1);
+
+}
+
+int somaVetor(int v[], int n){
+    if (n == 0){
+        return 0;
+    }
+
+    return v[n - 1] + somaVetor(v, n - 1);
+}
+
+int maior(int v[], int n) {
+    if (n == 1){
+        return v[0];
+    }
+
+    int anterior = maior (v, n - 1);
+
+    if (v[n - 1] > anterior){
+        return v[n - 1];
+    }
+
+    return anterior;
+}
+
+int fibonacci (int n){ 
+
+    if (n <= 1){
+        return n;
+    }
+
+    return fibonacci(n - 1) + fibonacci (n - 2);
+}
+
+int main()
+{
+
+    //contagem(3);
+    //int conta = somatorio(5);
+
+    //printf("%d", conta);
+
+    int vetor[5] = {10, 42, 583, 23, 13};
+
+    // printf("%lld\n", potencia(5, 8));
+    // printf("%d", somaVetor(vetor, sizeof(vetor) / 4));
+    // printf("%d", maior(vetor, sizeof(vetor) / 4));
+
+    printf("%d", fibonacci(7));
+
     return 0;
 }
